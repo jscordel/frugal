5758ed81f4ccf8489bc6a3b756f9cc645f810efb
Martin Vincendeau <vincendeau.martin@gmail.com>
2025-03-08 17:46:46 +0100

    Model and JSON for WIKI_LSTM_LSTM

--- apps/models/WIKI_LSTM_LSTM/main.py
+++ apps/models/WIKI_LSTM_LSTM/main.py
@@ -1,11 +1,14 @@
-# main.py
+from frugal.data_preproc.data import load_and_split_data, encoding_and_preproc
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import layers
+import gensim.downloader
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import EarlyStopping
-import gensim.downloader
-from frugal.data_preproc.data import load_and_split_data, encoding_and_preproc
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
 
 # Load and preprocess data
 X_train, X_test, y_train, y_test = load_and_split_data()
@@ -41,6 +44,10 @@ X_test_embedded = embed_sentences(w2v_model, X_test_preproc['quote'])
 X_train_pad = pad_sequences(X_train_embedded, maxlen=max_len, dtype='float32', padding='post')
 X_test_pad = pad_sequences(X_test_embedded, maxlen=max_len, dtype='float32', padding='post')
 
+# # LSTM PARAMS
+# lstm1 = 64
+# lstm2 = 32
+
 # Define the LSTM model
 model = Sequential([
     layers.Masking(mask_value=0., input_shape=(max_len, embedding_dim)),
@@ -51,7 +58,11 @@ model = Sequential([
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+    )
 
 # Early stopping
 early_stop = EarlyStopping(patience=3, restore_best_weights=True)
@@ -61,7 +72,7 @@ history = model.fit(
     X_train_pad,
     y_train_encoded,
     batch_size=32,
-    epochs=20,
+    epochs=1,
     validation_split=0.1,
     callbacks=[early_stop]
 )
@@ -71,5 +82,36 @@ loss, accuracy = model.evaluate(X_test_pad, y_test_encoded)
 print(f'Test loss: {loss:.4f}')
 print(f'Test accuracy: {accuracy:.4f}')
 
-# Summary of model
+# Predictions and reports
+predictions = model.predict(X_test_pad)
+predicted_classes = np.argmax(predictions, axis=1)
+classification_rep = classification_report(y_test_encoded, predicted_classes, output_dict=True)
+
+# Get current folder name
+model_folder_name = str(Path(__file__).parent).split("/")[-1]
+
+# Save results to JSON
+results = {
+    'model_name': model_folder_name,
+    'test_loss': loss,
+    'test_accuracy': accuracy,
+    'classification_report': classification_rep,
+    # 'parameters': {
+    #     "layer_1_model": "Masking",
+    #     "layer_2": f"LSTM {lstm1}"
+    # }
+}
+
+# # CONFUSION MATRIX
+# cm = confusion_matrix(y_test, predictions)
+# disp = ConfusionMatrixDisplay(confusion_matrix=cm) # display_labels=ordinal_values
+# disp.plot()
+# plt.show()
+
+json_file_path = f'/home/mvincendeau/code/jscordel/frugal/apps/evaluations/{model_folder_name}.json'
+
+with open(json_file_path, 'w') as f:
+    json.dump(results, f, indent=4)
+
+# Model summary
 model.summary()
