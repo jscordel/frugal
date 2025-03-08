f3ee26dae56683ef53290e802580e6f843481bdc
Jules Scordel <@users.noreply.github.com>
2025-03-08 15:14:44 +0100

    cleaning des commentaires

--- apps/models/jules_test_0/main.py
+++ apps/models/jules_test_0/main.py
@@ -2,18 +2,15 @@ from frugal.data.data import load_data
 from frugal.evaluation import evaluate, concatenate_evaluations
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-# -------------------NEW imports -----------------
 import os
 
 
 def main():
-    # ---------------NEW - Get model name --------------
     # Get the directory of the current script
     script_directory = os.path.dirname(os.path.abspath(__file__))
     # Get the name of the folder
     model_name = os.path.basename(script_directory)
     print(model_name)
-    # --------------------------------------------------
 
     # Load datasets and make data lists
     X_train, X_test, y_train, y_test = load_data()
